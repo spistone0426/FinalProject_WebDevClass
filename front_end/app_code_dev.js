66d6cac24eb907788b8a14c3c64a1cd6fd92b649
Salvatore Pistone <38148312+spistone0426@users.noreply.github.com>
2020-12-06 23:24:41 -0500

    Update app_code_dev.js

--- front_end/app_code_dev.js
+++ front_end/app_code_dev.js
@@ -1,29 +1,30 @@
+var url = window.location.hostname;
 var updateView = async (button) => {
 
     if (button.dataset.querytype == 'by_course_code') {
         let queryvalue = document.querySelector('#codeQuery').value;
-        api = `http://localhost:3000/api/by_course_code/${queryvalue}`;
+        api = `http://${url}/api/by_course_code/${queryvalue}`;
     }
 
     if (button.dataset.querytype == 'by_title') {
         let queryvalue = document.querySelector('#titleQuery').value;
-        api = `http://localhost:3000/api/by_title/${queryvalue}`;
+        api = `http://${url}/api/by_title/${queryvalue}`;
     }
 
     if (button.dataset.querytype == 'by_instructor') {
         let queryvalue = document.querySelector('#instructorQuery').value;
-        api = `http://localhost:3000/api/by_instructor/${queryvalue}`;
+        api = `http://${url}/api/by_instructor/${queryvalue}`;
     }
 
     if (button.dataset.querytype == 'by_level') {
         let queryvalue = document.querySelector('#levelQuery').value;
-        api = `http://localhost:3000/api/by_level/${queryvalue}`;
+        api = `http://${url}/api/by_level/${queryvalue}`;
     }
 
     if (button.dataset.querytype == 'combined_query') {
         let queryName = document.querySelector('#instructorquery2').value;
         let queryLevel = document.querySelector('#levelQuery2').value;
-        api = `http://localhost:3000/api/combined_query/${queryName}/${queryLevel}`;
+        api = `http://${url}/api/combined_query/${queryName}/${queryLevel}`;
     }
 
     const data = await fetch(api);
@@ -38,3 +39,4 @@ var render_view = (model) => {
     var html = template(model);
     document.querySelector("#results").innerHTML = html;
 }
+
